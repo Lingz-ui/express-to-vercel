9be1379ffa2551a6014c0de9bf297298fe7a1cd5
Charly Escalona <charly2221@hotmail.fr>
2022-05-31 21:17:57 +0200

    add users

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
+
+app.get('/users', (req, res) => {
+	axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+		return response.data;
+	})
+})
+
+
 app.listen(process.env.PORT || 3000);
 
 module.exports = app;
